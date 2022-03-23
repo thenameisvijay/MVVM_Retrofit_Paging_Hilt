0dcefadaa95c2fda74a80c21feea002f64122a8f
Vijay Vimalananda Guru <vg4029@gmail.com>
2022-03-23 15:45:21 +0530

    implementing testing for pagination

---
+++ app/src/androidTest/java/com/vj/mvvm_retrofitpaginghilt/DispatcherProvider.kt
@@ -0,0 +1,7 @@
+package com.vj.mvvm_retrofitpaginghilt
+
+/**
+* Created by Vijay on 23/03/2022.
+* https://github.com/thenameisvijay
+*/interface DispatcherProvider {
+}
---
+++ app/src/androidTest/java/com/vj/mvvm_retrofitpaginghilt/model/RepoPagingSourceTest.kt
@@ -0,0 +1,14 @@
+package com.vj.mvvm_retrofitpaginghilt.model
+
+import org.junit.Assert.*
+
+/**
+ * Created by Vijay on 23/03/2022.
+ * https://github.com/thenameisvijay
+ */
+class RepoPagingSourceTest {
+
+    @org.junit.Before
+    fun setUp() {
+    }
+}
